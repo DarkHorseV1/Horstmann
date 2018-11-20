20b3830e778d6d270bd7cbcc43f37332bd710d4a
DarkHorseV1 <darkhorse63189@gmail.com>
2018-11-20 15:01:02 +0300

    array sort

--- lambdatest2/src/cay/horstmann/LambdaTest.java
+++ lambdatest2/src/cay/horstmann/LambdaTest.java
@@ -9,5 +9,13 @@ public class LambdaTest
         String [] planets = new String [] {"Mercury","Venus","Earth",
         "Mars","Jupiter","Saturn","Uranus","Neptune"};
         System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted in dictionary order.");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted by length.");
+        Arrays.sort(planets,
+                (first, second) -> first.length() - second.length() );
+        System.out.println(Arrays.toString(planets));
+        
     }
 }
