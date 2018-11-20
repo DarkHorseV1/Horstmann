01f507ab43529cca46e7560a6863d018b67cce74
DarkHorseV1 <darkhorse63189@gmail.com>
2018-11-20 18:56:34 +0300

    sort

--- predicates1/src/ru/specialist/Predicates.java
+++ predicates1/src/ru/specialist/Predicates.java
@@ -11,14 +11,21 @@ public class Predicates
         List<Persons> persons = new ArrayList<>();
         persons.add(new Persons("Misha", 15));
         persons.add(new Persons("Masha", 44));
-        persons.add(new Persons("Mania", 21));
-        persons.add(new Persons("Lena", 19));
+        persons.add(new Persons("Mania", 11));
+        persons.add(new Persons("Lena", 17));
         persons.add(new Persons("Sasha", 35));
         persons.add(new Persons("Igori", 55));
         persons.add(new Persons("Oleg", 20));
 
+        /*императивный подход
         for(Persons p: persons)
             System.out.println(p);
-
+        */
+        
+        //persons.stream().forEach(p -> System.out.println(p));
+        persons.stream()
+                .filter(p -> p.getAge()>=18)
+                .sorted( (p1, p2) ->p1.getName().compareTo(p2.getName()))
+                .forEach(System.out::println);
     }
 }
