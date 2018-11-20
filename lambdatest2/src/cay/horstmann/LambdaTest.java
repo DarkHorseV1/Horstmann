eb490bfe16af9ed521743fc14405525a959af85f
DarkHorseV1 <darkhorse63189@gmail.com>
2018-11-20 14:49:52 +0300

    add array

---
+++ lambdatest2/src/cay/horstmann/LambdaTest.java
@@ -0,0 +1,13 @@
+package cay.horstmann;
+
+import java.util.Arrays;
+
+public class LambdaTest
+{
+    public static void main (String [] a)
+    {
+        String [] planets = new String [] {"Mercury","Venus","Earth",
+        "Mars","Jupiter","Saturn","Uranus","Neptune"};
+        System.out.println(Arrays.toString(planets));
+    }
+}
