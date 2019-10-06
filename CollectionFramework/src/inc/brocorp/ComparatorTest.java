653b83caf960ba61c46530c201ef649de30eeb46
DarkHorseV1 <darkhorse63189@gmail.com>
2019-10-07 01:50:09 +0300

    add thenComparing

--- CollectionFramework/src/inc/brocorp/ComparatorTest.java
+++ CollectionFramework/src/inc/brocorp/ComparatorTest.java
@@ -1,26 +1,63 @@
 package inc.brocorp;
 
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 
 public class ComparatorTest {
     public static void main(String[] args) {
-        MyComp mc = new MyComp();
-        TreeSet<String> ts = new TreeSet(mc.reversed());
-        ts.add("U");
-        ts.add("B");
-        ts.add("C");
-        ts.add("F");
-        ts.add("A");
+        TreeSet<Staff> ts = new TreeSet(new Staff().thenComparing((o1, o2) -> o1.getId().compareTo(o2.getId())));
+        /*
+        В качесте параметра для Tree Set передается кампаратор переопределнный в Staff чтобы сравнивать фамилии,
+        если фамилии одинаковые сравнение идет по именам.
+        Если имена и фамилии одинаковые с помощью метода thenComparing() создается цепочка компаратаров и
+        происходит сравнение по ID
+         */
+        ts.add(new Staff("004321", "Carpenter Bob", "Охранник"));
+        ts.add(new Staff("009112", "Carpenter Jack", "Продавец"));
+        ts.add(new Staff("002565", "Suzeen Dora", "Охранник"));
+        ts.add(new Staff("001890", "Maddison Owen", "Медик"));
+        ts.add(new Staff("003344", "Camel Kim", "Продавец"));
+        ts.add(new Staff("006712", "Carpenter Jack", "Деректор"));
+        ts.add(new Staff("001213", "Carpenter Abraham", "Заместитель"));
+        ts.add(new Staff("004001", "Abdornee Patrick", "Продавец"));
 
-        for (String o : ts) {
+
+        for (Staff o : ts) {
             System.out.println(o);
         }
     }
 }
 
-class MyComp implements Comparator<String> {
-    public int compare(String aStr, String bStr) {
-        return aStr.compareTo(bStr);
+class Staff implements Comparator<Staff> {
+    private String id;
+    private String name;
+    private String employeePosition;
+
+    public Staff(String id, String name, String employeePosition) {
+        this.id = id;
+        this.name = name;
+        this.employeePosition = employeePosition;
+    }
+
+    public Staff() {
+    }
+
+    @Override
+    public int compare(Staff o1, Staff o2) {
+        int i, j, k;
+        k = o1.name.compareTo(o2.name);
+        if (k == 0) {
+            i = o1.name.lastIndexOf(' ');
+            j = o2.name.lastIndexOf(' ');
+            return o1.name.substring(i).compareToIgnoreCase(o2.name.substring(j));
+        } else return k;
+    }
+
+    @Override
+    public String toString() {
+        return "ID = " + id + ", Name = " + name + ", Position = " + employeePosition + ";";
+    }
+
+    public String getId() {
+        return id;
     }
 }
